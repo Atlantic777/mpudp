22a287164ce606652be6917b8b504629a66d2635
Nikola Hardi <atlantic777@lugons.org>
2015-06-05 20:26:46 +0200

    Added placeholders for ip_utils tests

---
+++ include/tests/ip_utils.h
@@ -0,0 +1,11 @@
+#ifndef TEST_IP_UTILS_H
+#define TEST_IP_UTILS_H
+
+int init_ip_utils();
+int clean_ip_utils();
+
+void test_ip_build_packet();
+void test_ip_packet2chars();
+void test_ip_packet_len();
+
+#endif
---
+++ tests/ip_utils.c
@@ -0,0 +1,27 @@
+#include "tests/ip_utils.h"
+#include <CUnit/CUnit.h>
+
+int init_ip_utils()
+{
+    return 0;
+}
+
+int clean_ip_utils()
+{
+    return 0;
+}
+
+void test_ip_build_packet()
+{
+    CU_FAIL("Finish the test!");
+}
+
+void test_ip_packet2chars()
+{
+    CU_FAIL("Finish the test!");
+}
+
+void test_ip_packet_len()
+{
+    CU_FAIL("Finish the test!");
+}
