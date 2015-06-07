0c192e2da78f9a82acd2c7fb9d640bc30ce2a144
Atlantic777 <atlantic777@lugons.org>
2015-06-08 00:46:16 +0200

    Ask for host addr in nics.sh - fixes #26

--- scripts/nics.sh
+++ scripts/nics.sh
@@ -10,10 +10,13 @@ read i
 echo Choose id of network to join:
 read n
 
+echo Choose host address
+read h
+
 # do the magic
 ifconfig wlan$i down
 iwconfig wlan$i essid mpudp$n mode ad-hoc ap 00:00:00:00:00:0$n
-ifconfig wlan$i 192.168.10$n.$n
+ifconfig wlan$i 192.168.10$n.$h
 ifconfig wlan$i up
 
 # print results
