d97a663055c280c343ecca7296d6255f075a7265
Nikola Hardi <atlantic777@lugons.org>
2015-06-08 02:55:03 +0200

    Implemented basic packet sender routine for worker

--- include/mpudp_worker.h
+++ include/mpudp_worker.h
@@ -4,6 +4,7 @@
 #include <pthread.h>
 #include "mpudp_utils.h"
 #include "mpudp_monitor.h"
+#include <pcap.h>
 
 typedef struct worker worker_t;
 
@@ -15,9 +16,18 @@ struct worker {
     mpudp_buff_t tx_buff;
     monitor_t *m;
     int choke;
+    char *src_ip;
+    char *dst_ip;
+    uint16_t src_port;
+    uint16_t dst_port;
+    char *src_mac;
+    char *dst_mac;
+    pcap_t    *if_handle;
+    pcap_if_t *if_desc;
 };
 
 void* worker_tx_thread(void *arg);
 worker_t* spawn_worker(int, monitor_t*, float);
+int worker_send_packet(worker_t*, mpudp_packet_t*);
 
 #endif
--- src/mpudp_worker.c
+++ src/mpudp_worker.c
@@ -1,6 +1,9 @@
 #include "mpudp_worker.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "eth_utils.h"
+#include "ip_utils.h"
+#include "udp_utils.h"
 
 void* worker_tx_thread(void *arg)
 {
@@ -47,4 +50,32 @@ worker_t* spawn_worker(int id, monitor_t *m, float choke)
     pthread_create(&w->rx_thread_id, NULL, &worker_rx_thread, w);
 
     return w;
+}
+
+int worker_send_packet(worker_t *w, mpudp_packet_t *p)
+{
+    eth_frame_t eth_frame;
+    ip_packet_t ip_packet;
+    udp_dgram_t udp_dgram;
+
+    unsigned char *eth_payload, *ip_payload, *udp_payload;
+
+    int eth_len, ip_len, udp_len;
+
+    eth_compile_frame(&eth_frame, w->dst_mac, w->src_mac, ETH_TYPE_IP);
+    ip_build_packet(&ip_packet, w->src_ip, w->dst_ip);
+    udp_build_dgram_hdr(&udp_dgram, w->src_port, w->dst_port);
+
+    udp_set_data(&udp_dgram, p->payload, p->len);
+    udp_len = udp_dgram2chars(&udp_dgram, &udp_payload);
+
+    ip_set_data(&ip_packet, udp_payload, udp_len);
+    ip_len = ip_packet2chars(&ip_packet, &ip_payload);
+
+    eth_set_data(&eth_frame, ip_payload, ip_len);
+    eth_len = ip_frame2chars(&eth_frame, &eth_payload);
+
+    pcap_send_packet(w->if_handle, eth_payload, eth_len);
+
+    return 0;
 }
