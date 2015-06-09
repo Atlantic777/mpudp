ca1f0147fdbce6cd47ae5003ec4484509d6af7c1
Nikola Hardi <atlantic777@lugons.org>
2015-06-09 18:02:12 +0200

    Improved worker spawning in monitor - auto interface discovery

--- src/mpudp_monitor.c
+++ src/mpudp_monitor.c
@@ -2,29 +2,38 @@
 #include "mpudp_worker.h"
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 void* monitor_thread(void *arg)
 {
     monitor_t *m = (monitor_t*)arg;
+    int i;
 
-    worker_t *w1 = init_worker(1, "wlan2", m, 1);
+    char **iface_names_list;
+    int num_ifaces = pcapu_find_all_devs(&iface_names_list);
 
-    w1->dst_port = 8880;
-    w1->dst_mac = "00:c0:ca:59:70:0e";
-    w1->dst_ip = "192.168.101.2";
+    worker_t **workers = malloc(sizeof(worker_t*)*num_ifaces);
 
-    spawn_worker(w1);
+    for(i = 0; i < num_ifaces; i++)
+    {
+        printf("%d - %s\n", i, iface_names_list[i]);
+        workers[i] = init_worker(i, iface_names_list[i], m, 1);
+    }
 
-    worker_t *w2 = init_worker(2, "wlan4", m, 1.5);
+    workers[0]->dst_port = 8880;
+    workers[0]->dst_mac = "00:c0:ca:59:70:0e";
+    workers[0]->dst_ip = "192.168.101.2";
 
-    w2->dst_port = 8888;
-    w2->dst_mac = "00:0f:13:97:11:fa";
-    w2->dst_ip = "192.168.102.2";
+    workers[1]->dst_port = 8888;
+    workers[1]->dst_mac = "00:0f:13:97:11:fa";
+    workers[1]->dst_ip = "192.168.102.2";
 
-    spawn_worker(w2);
+    for(i = 0; i < num_ifaces; i++)
+        spawn_worker(workers[i]);
 
-    pthread_join(w1->tx_thread_id, NULL);
-    pthread_join(w2->tx_thread_id, NULL);
+    for(i = 0; i < num_ifaces; i++)
+        pthread_join(workers[i]->tx_thread_id, NULL);
+        pthread_join(workers[i]->rx_thread_id, NULL);
 }
 
 void init_monitor(monitor_t *m)
